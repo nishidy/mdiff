53d16211eba70b82413a370dbc308fc1cdb70d47
nishidy <nishidyatutokyo@gmail.com>
2016-04-30 15:43:40 +0900

    Updated tests and removed one function (not testing one).

--- mdiff.go
+++ mdiff.go
@@ -26,34 +26,24 @@ type result struct {
 	idxlist2 []uint32
 }
 
-func (rs *result) record(n uint32, idx uint32) {
+func (rs *result) Record(n uint32, idx uint32) {
 	if n == 1 {
-		rs.idxlist1 = append(rs.idxlist1, idx+1)
+		rs.idxlist1 = append(rs.idxlist1, idx)
 	} else {
-		rs.idxlist2 = append(rs.idxlist2, idx+1)
+		rs.idxlist2 = append(rs.idxlist2, idx)
 	}
 }
 
-func (rs *result) recordnone(n uint32) {
-	if n == 1 {
-		rs.idxlist1 = append(rs.idxlist1, 0)
-	} else {
-		rs.idxlist2 = append(rs.idxlist2, 0)
-	}
-}
-
-func (rs *result) diff(hashes1 hashelemlist, idx1 uint32, size1 uint32, hashes2 hashelemlist, idx2 uint32, size2 uint32) {
+func (rs *result) Diff(hashes1 hashelemlist, idx1 uint32, size1 uint32, hashes2 hashelemlist, idx2 uint32, size2 uint32) {
 
 	if idx1 == size1 && idx2 < size2 {
-		rs.recordnone(1)
-		rs.record(2, hashes2[idx2].idx)
-		rs.diff(hashes1, idx1, size1, hashes2, idx2+1, size2)
+		rs.Record(2, hashes2[idx2].idx)
+		rs.Diff(hashes1, idx1, size1, hashes2, idx2+1, size2)
 	}
 
 	if idx1 < size1 && idx2 == size2 {
-		rs.record(1, hashes1[idx1].idx)
-		rs.recordnone(2)
-		rs.diff(hashes1, idx1+1, size1, hashes2, idx2, size2)
+		rs.Record(1, hashes1[idx1].idx)
+		rs.Diff(hashes1, idx1+1, size1, hashes2, idx2, size2)
 	}
 
 	if idx1 == size1 || idx2 == size2 {
@@ -61,26 +51,24 @@ func (rs *result) diff(hashes1 hashelemlist, idx1 uint32, size1 uint32, hashes2 
 	}
 
 	if hashes1[idx1].hash == hashes2[idx2].hash {
-		rs.diff(hashes1, idx1+1, size1, hashes2, idx2+1, size2)
+		rs.Diff(hashes1, idx1+1, size1, hashes2, idx2+1, size2)
 	} else {
 		if (size1 - idx1) < (size2 - idx2) {
-			rs.recordnone(1)
-			rs.record(2, hashes2[idx2].idx)
-			rs.diff(hashes1, idx1, size1, hashes2, idx2+1, size2)
+			rs.Record(2, hashes2[idx2].idx)
+			rs.Diff(hashes1, idx1, size1, hashes2, idx2+1, size2)
 		} else if (size1 - idx1) > (size2 - idx2) {
-			rs.record(1, hashes1[idx1].idx)
-			rs.recordnone(2)
-			rs.diff(hashes1, idx1+1, size1, hashes2, idx2, size2)
+			rs.Record(1, hashes1[idx1].idx)
+			rs.Diff(hashes1, idx1+1, size1, hashes2, idx2, size2)
 		} else {
-			rs.record(1, hashes1[idx1].idx)
-			rs.record(2, hashes2[idx2].idx)
-			rs.diff(hashes1, idx1+1, size1, hashes2, idx2+1, size2)
+			rs.Record(1, hashes1[idx1].idx)
+			rs.Record(2, hashes2[idx2].idx)
+			rs.Diff(hashes1, idx1+1, size1, hashes2, idx2+1, size2)
 		}
 	}
 
 }
 
-func (rs *result) stat(n uint32, fd *os.File) {
+func (rs *result) Stat(n uint32, fd *os.File) {
 
 	var fname string
 	var idxlist []uint32
@@ -95,7 +83,7 @@ func (rs *result) stat(n uint32, fd *os.File) {
 
 	scanner := bufio.NewScanner(fd)
 	var idx = 0
-	var line uint32 = 1 // 0 is reserved for none
+	var line uint32 = 0
 	for scanner.Scan() {
 		str := scanner.Text()
 		if line == idxlist[idx] {
@@ -110,25 +98,29 @@ func (rs *result) stat(n uint32, fd *os.File) {
 
 }
 
-func hashID(s string) uint64 {
+func HashID(s string) uint64 {
 	h := fnv.New64a()
 	h.Write([]byte(s))
 	sum := h.Sum64()
 	return sum
 }
 
-func hashLines(fd *os.File, hashes *hashelemlist, ch chan int) {
+func HashLines(fd *os.File, hashes *hashelemlist, ch chan int) {
 	scanner := bufio.NewScanner(fd)
 	var idx uint32 = 0
 	for scanner.Scan() {
 		str := scanner.Text()
-		*hashes = append(*hashes, hashelem{idx, hashID(str)})
+		*hashes = append(*hashes, hashelem{idx, HashID(str)})
 		idx += 1
 	}
 	ch <- 0
 }
 
-func runDiff(rs *result) {
+func sortByHash(hashes *hashelemlist) {
+	sort.Sort(*hashes)
+}
+
+func RunDiff(rs *result) {
 
 	fd1, err := os.Open(rs.fname1)
 	if err != nil {
@@ -146,25 +138,25 @@ func runDiff(rs *result) {
 	ch2 := make(chan int, 1)
 
 	hashes1 := make(hashelemlist, 0, 10000)
-	go hashLines(fd1, &hashes1, ch1)
+	go HashLines(fd1, &hashes1, ch1)
 
 	hashes2 := make(hashelemlist, 0, 10000)
-	go hashLines(fd2, &hashes2, ch2)
+	go HashLines(fd2, &hashes2, ch2)
 
 	<-ch1
 	<-ch2
 
-	sort.Sort(hashes1)
-	sort.Sort(hashes2)
+	sortByHash(&hashes1)
+	sortByHash(&hashes2)
 
 	size1 := uint32(len(hashes1))
 	size2 := uint32(len(hashes2))
 
-	rs.diff(hashes1, 0, size1, hashes2, 0, size2)
+	rs.Diff(hashes1, 0, size1, hashes2, 0, size2)
 
 }
 
-func runStat(rs *result) {
+func RunStat(rs *result) {
 
 	fd1, err := os.Open(rs.fname1)
 	if err != nil {
@@ -178,8 +170,8 @@ func runStat(rs *result) {
 	}
 	defer fd2.Close()
 
-	rs.stat(1, fd1)
-	rs.stat(2, fd2)
+	rs.Stat(1, fd1)
+	rs.Stat(2, fd2)
 
 }
 
@@ -200,6 +192,6 @@ func main() {
 		make([]uint32, 0, 1000),
 	}
 
-	runDiff(rs)
-	runStat(rs)
+	RunDiff(rs)
+	RunStat(rs)
 }
--- mdiff_test.go
+++ mdiff_test.go
@@ -11,7 +11,7 @@ func TestRecord(t *testing.T) {
 	var rs *result
 
 	rs = &result{"a", "b", lst, lst}
-	rs.Record(1, 3)
+	rs.Record(1, 4)
 	if !reflect.DeepEqual(rs.idxlist1, []uint32{1, 2, 3, 4}) {
 		t.Error("The first list not recorded.")
 	}
@@ -20,7 +20,7 @@ func TestRecord(t *testing.T) {
 	}
 
 	rs = &result{"a", "b", lst, lst}
-	rs.Record(2, 3)
+	rs.Record(2, 4)
 	if !reflect.DeepEqual(rs.idxlist1, lst) {
 		t.Error("The first list not remained.")
 	}
@@ -31,55 +31,134 @@ func TestRecord(t *testing.T) {
 
 }
 
-func TestRecordNone(t *testing.T) {
-
-	lst := []uint32{1, 2, 3}
-	var rs *result
-
-	rs = &result{"a", "b", lst, lst}
-	rs.RecordNone(1)
-	if !reflect.DeepEqual(rs.idxlist1, []uint32{1, 2, 3, 0}) {
-		t.Error("The first list not recorded.")
-	}
-	if !reflect.DeepEqual(rs.idxlist2, lst) {
-		t.Error("The second list not remained.")
-	}
-
-	rs = &result{"a", "b", lst, lst}
-	rs.RecordNone(2)
-	if !reflect.DeepEqual(rs.idxlist1, lst) {
-		t.Error("The first list not recorded.")
-	}
-	if !reflect.DeepEqual(rs.idxlist2, []uint32{1, 2, 3, 0}) {
-		t.Error("The second list not remained.")
-	}
-
-}
-
-func TestDiff(t *testing.T) {
+func TestDiff1(t *testing.T) {
 
 	var rs *result
 
 	h1 := []hashelem{
-		hashelem{0, 1},
-		hashelem{1, 2},
-		hashelem{2, 3},
+		hashelem{0, 10},
+		hashelem{1, 20},
+		hashelem{2, 30},
+	}
+
+	rs = &result{"a", "b", []uint32{}, []uint32{}}
+	rs.Diff(h1, 0, uint32(len(h1)), h1, 0, uint32(len(h1)))
+
+	if !reflect.DeepEqual(rs.idxlist1, []uint32{}) {
+		t.Error("The first result is not expected.", rs.idxlist1)
+	}
+	if !reflect.DeepEqual(rs.idxlist2, []uint32{}) {
+		t.Error("The second result is not expected.", rs.idxlist2)
+	}
+
+}
+
+func TestDiff2(t *testing.T) {
+
+	var rs *result
+
+	h1 := []hashelem{
+		hashelem{0, 10},
+		hashelem{1, 20},
+		hashelem{2, 30},
 	}
 
 	h2 := []hashelem{
-		hashelem{0, 1},
-		hashelem{1, 2},
-		hashelem{2, 4},
+		hashelem{0, 10},
+		hashelem{1, 20},
+		hashelem{2, 40},
 	}
 
 	rs = &result{"a", "b", []uint32{}, []uint32{}}
 	rs.Diff(h1, 0, uint32(len(h1)), h2, 0, uint32(len(h2)))
 
-	if !reflect.DeepEqual(rs.idxlist1, []uint32{3}) {
+	if !reflect.DeepEqual(rs.idxlist1, []uint32{2}) {
 		t.Error("The first result is not expected.", rs.idxlist1)
 	}
-	if !reflect.DeepEqual(rs.idxlist2, []uint32{3}) {
+	if !reflect.DeepEqual(rs.idxlist2, []uint32{2}) {
 		t.Error("The second result is not expected.", rs.idxlist2)
+	}
+
+}
+
+func TestDiff3(t *testing.T) {
+
+	var rs *result
+
+	h1 := []hashelem{
+		hashelem{0, 10},
+		hashelem{1, 20},
+		hashelem{2, 30},
+	}
+
+	h2 := []hashelem{
+		hashelem{0, 10},
+		hashelem{1, 50},
+		hashelem{2, 30},
+	}
+
+	rs = &result{"a", "b", []uint32{}, []uint32{}}
+	rs.Diff(h1, 0, uint32(len(h1)), h2, 0, uint32(len(h2)))
+
+	if !reflect.DeepEqual(rs.idxlist1, []uint32{1}) {
+		t.Error("The first result is not expected.", rs.idxlist1)
+	}
+	if !reflect.DeepEqual(rs.idxlist2, []uint32{1}) {
+		t.Error("The second result is not expected.", rs.idxlist2)
+	}
+
+}
+
+func TestDiff4(t *testing.T) {
+
+	var rs *result
+
+	h1 := []hashelem{
+		hashelem{0, 10},
+		hashelem{1, 20},
+		hashelem{2, 30},
+	}
+
+	h2 := []hashelem{
+		hashelem{0, 10},
+		hashelem{1, 20},
+		hashelem{2, 30},
+		hashelem{3, 40},
+		hashelem{4, 50},
+	}
+
+	rs = &result{"a", "b", []uint32{}, []uint32{}}
+	rs.Diff(h1, 0, uint32(len(h1)), h2, 0, uint32(len(h2)))
+
+	if !reflect.DeepEqual(rs.idxlist1, []uint32{}) {
+		t.Error("The first result is not expected.", rs.idxlist1)
+	}
+	if !reflect.DeepEqual(rs.idxlist2, []uint32{3, 4}) {
+		t.Error("The second result is not expected.", rs.idxlist2)
+	}
+
+}
+
+func TestSortByHash(t *testing.T) {
+
+	h1 := hashelemlist{
+		hashelem{0, 20},
+		hashelem{1, 10},
+		hashelem{2, 40},
+		hashelem{3, 30},
+	}
+
+	sortByHash(&h1)
+
+	a1 := hashelemlist{
+		hashelem{1, 10},
+		hashelem{0, 20},
+		hashelem{3, 30},
+		hashelem{2, 40},
+	}
+
+	if !reflect.DeepEqual(h1, a1) {
+		t.Error("Not sorted by hash properly.")
 	}
 
 }
